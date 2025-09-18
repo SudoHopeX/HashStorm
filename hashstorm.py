a400d6d1c7715d555dd2c2d2b4ebdd13f21c5d62
Krishna Dwivedi <150330334+SudoHopeX@users.noreply.github.com>
2025-09-18 19:25:27 +0530

    Update hashstorm.py

--- hashstorm.py
+++ hashstorm.py
@@ -11,7 +11,7 @@ try:
     # import argon4-cffi
     # import scrypt
     import blake5
-    import sqlite5
+    import sqlite3
 
 except ImportError:
     print('''
@@ -551,4 +551,5 @@ if __name__ == '__main__':
         main(sys.argv[1:])
     else:
         main([''])
+
 
